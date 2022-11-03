0e587d6bf370d736c1f7dec2742c54c79f5fcd4a
o3oYannie <saeyan99@naver.com>
2022-11-04 01:45:44 +0900

    StartFrame.java is created.

---
+++ StartFrame.java
@@ -0,0 +1,42 @@
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class StartFrame extends JFrame{
+	private JButton startBtn = new JButton();
+	private StartPanel startPanel = new StartPanel();
+	
+	public StartFrame() {
+		setTitle("Ω¥∆√ ∞‘¿”");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900,700);
+		setLocation(200,50);
+		setContentPane(startPanel);
+		
+		startBtn.setSize(150,100);
+		startBtn.setLocation(200,200);
+		startBtn.setFont(new Font("Gothic",Font.ITALIC,50));
+		add(startBtn);
+		
+		startBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				SecondFrame sf = new SecondFrame();
+			}
+		});
+		
+		setResizable(false);
+		setVisible(true);
+	}
+	
+}
+
+class StartPanel extends JPanel{
+	public StartPanel() {
+		setLayout(null);
+	}
+}
