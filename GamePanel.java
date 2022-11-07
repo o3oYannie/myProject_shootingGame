8852f8ace6de869814c24476dcb052804f8fda13
o3oYannie <saeyan99@naver.com>
2022-11-07 19:02:50 +0900

    GamePanel, LifePanel, ScorePanel is modified

--- GamePanel.java
+++ GamePanel.java
@@ -1,13 +1,21 @@
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import java.awt.*;
 public class GamePanel extends JPanel{
 	
-	public JLabel text = new JLabel(); 
+	public JLabel text = new JLabel();  
 	private LifePanel lPanel = null;
 	private ScorePanel sPanel = null;
-	
+	private ImageIcon userImage = new ImageIcon("sangsangBoogie.png");
+	private JLabel user = new JLabel(); //움직이는 캐릭터
+
 	public GamePanel() {
+		
+		this.setBackground(Color.WHITE);
+		setLayout(null);
+		
+		setVisible(true);
 		
 	}
 }
--- LifePanel.java
+++ LifePanel.java
@@ -8,6 +8,8 @@ public class LifePanel extends JPanel {
 	private ImageIcon lifeHeart = new ImageIcon("lifeheart.png");
 	private JLabel lifeText = new JLabel("Life : "); 
 	private JLabel lifeLabel1 = new JLabel();
+	private JLabel lifeLabel2 = new JLabel();
+	private JLabel lifeLabel3 = new JLabel();
 	//private Image lifeHeartImg = lifeHeart.getImage();
 	
 	public LifePanel() {
@@ -16,20 +18,26 @@ public class LifePanel extends JPanel {
 		
 		lifeText.setBounds(10,0,150,50);
 		lifeText.setBackground(Color.WHITE);
-		lifeText.setOpaque(true);
 		lifeText.setFont(new Font("Gothic",Font.ITALIC,30));
 		add(lifeText);
 		
 		lifeLabel1.setIcon(lifeHeart);
 		lifeLabel1.setSize(50,50);
-		lifeLabel1.setLocation(10,0);
+		lifeLabel1.setLocation(90,0);
 		add(lifeLabel1);
+
+		lifeLabel2.setIcon(lifeHeart);
+		lifeLabel2.setSize(50,50);
+		lifeLabel2.setLocation(120,0);
+		add(lifeLabel2);
+		
+		lifeLabel3.setIcon(lifeHeart);
+		lifeLabel3.setSize(50,50);
+		lifeLabel3.setLocation(150,0);
+		add(lifeLabel3);
 		
 		
 		setVisible(true);
 	}
 	
-	public void paintComponent(Graphics g) {
-		//g.drawIamge();
-	}
 }
