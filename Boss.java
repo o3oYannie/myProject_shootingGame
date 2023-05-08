675609436335b81bf7277295b10662cbbb458e0a
o3oYannie <saeyan99@naver.com>
2023-05-08 19:23:12 +0900

    add boss life bar

--- Boss.java
+++ Boss.java
@@ -1,15 +1,17 @@
 public class Boss {
 	int x;
 	int y;
+	int life=10;
 	boolean mv=true;
+	
 	Boss(int x,int y){
 		this.x=x;
 		this.y=y;
 	}
+	
 	public void move() {
 		if(mv==true)
 			x-=3;
-		
 	}
 	
 }
